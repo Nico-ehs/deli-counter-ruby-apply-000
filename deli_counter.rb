884a847952265c9021d8dffc465f08db5e499609
Nico-ehs <nico.ehs05@gmail.com>
2018-08-16 18:12:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 def now_serving(deli)
   if deli==[] then return puts("There is nobody waiting to be served!") end
   puts("Currently serving #{deli.shift}.")
-# end
+end
 
 # def take_a_number(deli, name) puts("Welcome, #{deli.push(name).last}. You are number #{deli.size} in line.")end
 # def line(deli) puts(deli==[] ? "The line is currently empty." : "The line is currently:#{deli.map.with_index{ |name, i| " #{i+1}. #{name}" }.join}")end
