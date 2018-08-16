7a5503162be479ef51c63f7a169de2e1a8faf81a
Nico-ehs <nico.ehs05@gmail.com>
2018-08-16 18:10:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,19 @@
 # Write your code here.
 
-# def take_a_number(deli, name)
-#   deli.push(name)
-#   puts("Welcome, #{name}. You are number #{deli.size} in line.")
-# end
+def take_a_number(deli, name)
+  deli.push(name)
+  puts("Welcome, #{name}. You are number #{deli.size} in line.")
+end
 
-# def line(deli)
-#   if deli==[] then return puts("The line is currently empty.") end
-#   puts("The line is currently:#{deli.map.with_index{ |name, i| " #{i+1}. #{name}" }.join}")
-# end
+def line(deli)
+  if deli==[] then return puts("The line is currently empty.") end
+  puts("The line is currently:#{deli.map.with_index{ |name, i| " #{i+1}. #{name}" }.join}")
+end
 
-# def now_serving(deli)
-#   if deli==[] then return puts("There is nobody waiting to be served!") end
-#   puts("Currently serving #{deli.shift}.")
-# end
+def now_serving(deli)
+  if deli==[] then return puts("There is nobody waiting to be served!") end
+  puts("Currently serving #{deli.shift}.")
+end
 
 def take_a_number(deli, name) puts("Welcome, #{deli.push(name).last}. You are number #{deli.size} in line.")end
 def line(deli) puts(deli==[] ? "The line is currently empty." : "The line is currently:#{deli.map.with_index{ |name, i| " #{i+1}. #{name}" }.join}")end
