043db6aa9f6a18eb159c6c00d86ddc98cd83d5ec
Nico-ehs <nico.ehs05@gmail.com>
2018-08-16 18:19:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,27 @@
 # Write your code here.
 
+class deli
+  
+  def intilize
+    @line=[]
+    @served=0
+  end
+
 def take_a_number(deli)
-  puts("Welcome, You are number #{deli.push(deli.last+1).last} in line.")
+  puts("Welcome, You are number #{deli==[] ?  : deli.push(deli.last+1).last} in line.")
 end
 
 def line(deli)
   if deli==[] then return puts("The line is currently empty.") end
-  puts("The line is currently: #{deli.size} long")
+  puts("The line is currently: #{deli.size} #{dile.size==1? "person ": "people" } long{}")
 end
 
 def now_serving(deli)
   if deli==[] then return puts("There is nobody waiting to be served!") end
   puts("Currently serving number #{deli.shift}.")
 end
+
+
 
 # def take_a_number(deli, name) puts("Welcome, #{deli.push(name).last}. You are number #{deli.size} in line.")end
 # def line(deli) puts(deli==[] ? "The line is currently empty." : "The line is currently:#{deli.map.with_index{ |name, i| " #{i+1}. #{name}" }.join}")end
